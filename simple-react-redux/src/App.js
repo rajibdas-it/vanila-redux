8cb6ce846ac736e82156fc19b4304d40b7b1ed65
rajibdas-it <rajibrad@gmail.com>
2023-02-22 12:52:44 +0600

    state management with normal way in react

--- simple-react-redux/src/App.js
+++ simple-react-redux/src/App.js
@@ -1,12 +1,19 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Counter from "./component/Counter";
 
 function App() {
   return (
-    <>
-      <Counter />
-    </>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <div class="max-w-md mx-auto mt-10 space-y-5">
+        <Counter />
+        <Counter />
+        <Counter />
+        <Counter />
+      </div>
+    </div>
   );
 }
 
--- simple-react-redux/src/component/Counter.js
+++ simple-react-redux/src/component/Counter.js
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return (
-    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
-        Simple Counter Application
-      </h1>
+  const [count, setCount] = useState(0);
 
-      <div class="max-w-md mx-auto mt-10 space-y-5">
-        <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-          <div class="text-2xl font-semibold" id="value">
-            0
-          </div>
-          <div class="flex space-x-3">
-            <button
-              class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-              id="increment"
-            >
-              Increment
-            </button>
-            <button
-              class="bg-red-400 text-white px-3 py-2 rounded shadow"
-              id="decrement"
-            >
-              Decrement
-            </button>
-          </div>
-        </div>
+  const handleIncrement = () => {
+    // setCount(count + 1); normal way.
+    setCount((prevCount) => prevCount + 1); //paranormal way
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
+  return (
+    <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+      <div class="text-2xl font-semibold" id="value">
+        {count}
+      </div>
+      <div class="flex space-x-3">
+        <button
+          class="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+          id="increment"
+          onClick={handleIncrement}
+        >
+          Increment
+        </button>
+        <button
+          class="bg-red-400 text-white px-3 py-2 rounded shadow"
+          id="decrement"
+          onClick={handleDecrement}
+        >
+          Decrement
+        </button>
       </div>
     </div>
   );
